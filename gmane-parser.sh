b80dbc5d773c05b0182600d00716704db24f898f
Samuli Seppänen <samuli@openvpn.net>
2014-04-22 15:54:09 +0300

    Various improvements and cleanups
    Signed-off-by: krzee
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- gmane-parser.sh
+++ gmane-parser.sh
@@ -5,6 +5,8 @@
 # easily parseable lists of postings using URLs like this:
 #
 # http://news.gmane.org/group/gmane.network.openvpn.user/last=0/force_load=very/?page=2&action=--Action--
+# doctored up a little by krzee on 4-20-14 (it was hurting my eyes)
+
 
 # Variables
 BASEURL="http://news.gmane.org/group/gmane.network"
@@ -16,38 +18,35 @@ AUTHORSFILE=/tmp/authors.txt
 RESULTSFILE=/tmp/results.txt
 
 # Usage
-if [ "$2" == "" ]; then
-    echo "Usage: gmane_parser.sh list pages"
+help(){
+    echo "Usage: gmane-parser.sh list pages"
     echo
-    echo "Example: gmane_parser.sh openvpn.user 20"
+    echo "Example: gmane-parser.sh openvpn.user 20"
     exit 1
-fi
+}
+
+[ -z $2 ] && help
 
 # Remove files left from previous runs
-rm -f "$TEMPFILE"
-rm -f "$POSTSFILE"
-rm -f "$AUTHORSFILE"
-rm -f "$RESULTSFILE"
+rm -f "$TEMPFILE" "$POSTSFILE" "$AUTHORSFILE" "$RESULTSFILE"
 
 # Load and save the webpages we're interested in into a temporary file
 PAGE=0
 while [ $PAGE -le $LASTPAGE ]; do
-    wget "$BASEURL.$LIST/last=0/force_load=very/?page=$PAGE&action=--Action--" -O - >> "$TEMPFILE"
+    wget -q "$BASEURL.$LIST/last=0/force_load=very/?page=$PAGE&action=--Action--" -O - >> "$TEMPFILE"
     PAGE=$(( $PAGE + 1 ))
 done
 
 # Get author names and post counts
-cat "$TEMPFILE" |grep "class=\"unread\""|cut -d ">" -f 5|cut -d "<" -f 1 > "$POSTSFILE"
-cat "$POSTSFILE"|sort|uniq|grep -v "^\.$"|grep -x "^.*[[:alpha:]].*$" > "$AUTHORSFILE"
+grep "class=\"unread\"" "$TEMPFILE"|cut -d ">" -f 5|cut -d "<" -f 1 > "$POSTSFILE"
+sort -u "$POSTSFILE"|grep -v "^\.$"|grep -x "^.*[[:alpha:]].*$" > "$AUTHORSFILE"
 
 # Count posts by author
-cat "$AUTHORSFILE"| while read AUTHOR; do
-    POSTCOUNT=`grep -c "^$AUTHOR$" "$POSTSFILE"`
+while read AUTHOR; do
+    POSTCOUNT=$(grep -c "^$AUTHOR$" "$POSTSFILE")
 
     # Add zero padding to post counts if necessary
-    if [ ${#POSTCOUNT} -eq 3 ]; then
-        POSTS=$POSTCOUNT
-    elif [ ${#POSTCOUNT} -eq 2 ]; then
+    if   [ ${#POSTCOUNT} -eq 2 ]; then
         POSTS=0$POSTCOUNT
     elif [ ${#POSTCOUNT} -eq 1 ]; then
         POSTS=00$POSTCOUNT
@@ -55,5 +54,7 @@ cat "$AUTHORSFILE"| while read AUTHOR; do
         POSTS=$POSTCOUNT
     fi
 
-    echo "$POSTS: $AUTHOR" >> "$RESULTSFILE"
-done
+    echo "$POSTS: $AUTHOR" >> "$RESULTSFILE".2
+done < "$AUTHORSFILE"
+sort -r "$RESULTSFILE".2 > "$RESULTSFILE"
+rm "$RESULTSFILE".2
